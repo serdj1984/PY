7c7edb5dc4994799e2708b3f25ab2b7189d7f57b
serdj1984 <139622188+serdj1984@users.noreply.github.com>
2023-07-26 21:25:53 +0300

    Update main.py

--- DZ/ДЗ_ПИТОН_23/main.py
+++ DZ/ДЗ_ПИТОН_23/main.py
@@ -1,3 +1,7 @@
+a="""
+#1	Необходимо реализовать для класса Student атрибут id, который должен присваиваться случайным образом
+#2	Сделать сортировку списка students по убыванию атрибута id (от 100 до 1 к примеру)
+"""
 
 
 from Student import Student
@@ -5,7 +9,6 @@ import random as r
 students = []
 names = ['Ivan Ivanov','Petr Petrov','Vanya Ivanov']
 groups = ['p123','q24','q333','t96']
-     
 
 for i in range(10):
     student = Student(names[r.randint(0,2)],#random.randint(0,2) -выбирает случайный индекс
@@ -15,30 +18,38 @@ for i in range(10):
     students.append(student)
 
 
-
-# print("\n#--------------------#\n") 
-
+print("\n#---------- показать всех студентов ----------#\n")
 for student in students:
-        student.show()    
-print("\n#--------------------#\n") 
+        student.show()
+print("\n\n") 
 
-#---------------------- отсортировать студентов по среднему баллу
+# --- показать всех студентов из одной группы, -> необхоимо реализовать геттер для группы --- #
+print("\n#---------- показать всех студентов из одной группы ----------#\n")
+group_for_search = 'p123'
+for student in students:
+    if student.getgroup == group_for_search:
+        student.show()
+print("\n\n") 
+   
+
+# ---------------------- отсортировать студентов по среднему баллу -------------------------- #
 for j in range(len(students)):# за повторный проход
     for i in range(len(students)-1):
         if students[i].getgpa < students[i+1].getgpa:
             students[i], students[i+1] = students[i+1], students[i]
-print("\n#--------------------#\n") 
-
+            
+print("\n#---------- отсортировать студентов по среднему баллу ----------#\n") 
 for student in students:
         student.show()
+print("\n\n") 
 
-
-#---------------------- отсортировать студентов по убыванию id
+#-------------------------- отсортировать студентов по убыванию id -------------------------- #
 for j in range(len(students)):# за повторный проход
     for i in range(len(students)-1):
         if students[i].getid < students[i+1].getid:
             students[i], students[i+1] = students[i+1], students[i]
-print("\n#--------------------#\n") 
 
+print("\n#---------- отсортировать студентов по убыванию id ----------#\n")            
 for student in students:
         student.show()
+
