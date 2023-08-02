d86612edebfa414c200a2bb5b0a5f83b46b462c9
serdj1984 <139622188+serdj1984@users.noreply.github.com>
2023-08-02 22:50:52 +0300

    Update Product.py

--- DZ/ДЗ_ПИТОН_24/Product.py
+++ DZ/ДЗ_ПИТОН_24/Product.py
@@ -4,10 +4,10 @@ class Product:
         if not(type_p in ['одежда', 'обувь', 'украшение']):  raise 'type is not одежда, обувь, украшение'# type_p == 'одежда' or type_p == 'обувь'
         if cost < 0: raise 'cost is below zero' # raise вызываем ошибку
         if not(type(cost) in [int, float]): raise 'cost is not int or float'
-        
         self.__name = name
         self.__type = type_p
         self.__cost = cost
+        
     def show(self):
         print(self.__name ,
         self.__type,
