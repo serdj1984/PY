094e93e5b828712bc700f139a0b4f77bb6061160
serdj1984 <139622188+serdj1984@users.noreply.github.com>
2023-09-03 12:31:33 +0300

    Update tabl.sql

--- DZ/ДЗ_SQL_32/tabl.sql
+++ DZ/ДЗ_SQL_32/tabl.sql
@@ -1,1 +1,14 @@
+create database if not exists lessondb
+use lessondb
+create table if not exists doctors(
+names varchar(50) not null,
+post varchar(20) not null,
+salary int(12)
+);
+insert doctors values ("Гебхардт Карл Франц", "Хирург", 500000),
+("Эдриан Фернхэм", "Психолог", 300000),
+("Николаева Лариса Ивановна","Терапевт", 100000),
+("Коновалов Андрей Александрович", "Хирург", 400000);
 
+SELECT * FROM lessondb.doctors;
+SELECT * FROM lessondb.doctors WHERE post="Хирург";
