fb7052bb6f194a76edbbe2d855ade1871528df3a
serdj1984 <139622188+serdj1984@users.noreply.github.com>
2023-09-16 22:39:22 +0300

    Update ДЗ_SQL_34.sql

--- DZ/ДЗ_SQL_34/ДЗ_SQL_34.sql
+++ DZ/ДЗ_SQL_34/ДЗ_SQL_34.sql
@@ -1,22 +1,26 @@
-create database if not exists cafedb;
-use cafedb;
+/* -------------------- Создаем БД ----------------------- */
+create database if not exists cafedb;                   -- */
+use cafedb; -- переходим в базу                         -- */
 
-create table if not exists barista(
-id integer primary key auto_increment,
-names varchar(50) not null
-);
-create table if not exists coffee(
-id integer primary key auto_increment,
-appellation varchar(50) not null
-);
+/* ----------------- Создаем таблицы --------------------- */
+create table if not exists barista(                     -- */
+id integer primary key auto_increment,                  -- */
+names varchar(50) not null                              -- */
+);                                                      -- */
+create table if not exists coffee(                      -- */
+id integer primary key auto_increment,                  -- */
+appellation varchar(50) not null                        -- */
+);                                                      -- */
+                                                        -- */
+create table if not exists laborer(                     -- */
+barista_id integer,                                     -- */
+foreign key (barista_id) references barista(id),        -- */
+coffee_id integer,                                      -- */
+foreign key (coffee_id) references coffee(id),          -- */
+`data_time` date not null                               -- */
+);                                                      -- */
 
-create table if not exists laborer(
-barista_id integer,
-foreign key (barista_id) references barista(id),
-coffee_id integer,
-foreign key (coffee_id) references coffee(id),
-`data_time` date not null
-);
+/* ------------------------------- Наполняем таблицы ----------------------------------- */
 -- insert into barista (names) values ("Вася"), ("Петя");
 -- insert into coffee (appellation) values  ("Эспрессо"), ("Капучино"), ("Латте"), ("Чай");
 -- insert into laborer (barista_id, coffee_id, data_time) values ('1','1','2023-09-10'),
@@ -27,10 +31,12 @@ foreign key (coffee_id) references coffee(id),
 -- ('2','3','2023-09-11'),
 -- ('2','4','2023-09-11');
 
+/* ------------ Какой бариста на смене ----------------- */
 -- SELECT laborer.data_time, barista.names FROM barista 
 -- INNER JOIN laborer ON barista.id = laborer.barista_id
 
-SELECT laborer.data_time, coffee.appellation FROM coffee 
-INNER JOIN laborer ON coffee.id = laborer.coffee_id;
+/* ------------ Какие кофе продали на смене ------------ */
+-- SELECT laborer.data_time, coffee.appellation FROM coffee 
+-- INNER JOIN laborer ON coffee.id = laborer.coffee_id;
 
 -- SELECT * FROM cafedb.laborer
